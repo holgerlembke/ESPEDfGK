18b9dc6f033e6849d5ec84b96e2a6fcb86313a64
Holger Lembke <lembke@gmail.com>
2023-05-17 21:22:50 +0200

    up

--- AvalonTools.cs
+++ AvalonTools.cs
@@ -1,10 +1,5 @@
 ﻿using ICSharpCode.AvalonEdit.Rendering;
 using ICSharpCode.AvalonEdit;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Media;
 using System.Windows;
 
@@ -13,7 +8,6 @@ namespace ESPEDfGK
     public class HighlightCurrentLineBackgroundRenderer : IBackgroundRenderer
     {
         private TextEditor _editor;
-        // public List<DiffPiece> Lines { get; set; } /* DiffPlex model's lines */
         public int LineNumber;
 
         public HighlightCurrentLineBackgroundRenderer(TextEditor editor)
@@ -35,7 +29,7 @@ namespace ESPEDfGK
 
             if (LineNumber > 0)
             {
-                Brush highlight = Brushes.LightGreen; //Brushes.Transparent;
+                Brush highlight = Brushes.LightGreen; 
                 var currentLine = _editor.Document.GetLineByNumber(LineNumber);
                 foreach (var rect in BackgroundGeometryBuilder.GetRectsForSegment(textView, currentLine))
                 {
@@ -43,44 +37,6 @@ namespace ESPEDfGK
                         null, new Rect(rect.Location, new Size(rect.Width, rect.Height)));
                 }
             }
-
-            /*
-            for (var i = 0; i < Lines.Count; i++)
-            {
-                var diffLine = Lines[i];
-                if (!string.IsNullOrEmpty(diffLine.Text))
-                {
-                    var highlight = GetLineBackgroundBrush(diffLine);
-                    var currentLine = _editor.Document.GetLineByNumber(i + 1);
-                    foreach (var rect in BackgroundGeometryBuilder.GetRectsForSegment(textView, currentLine))
-                    {
-                        drawingContext.DrawRectangle(highlight, null, new Rect(rect.Location, new Size(rect.Width, rect.Height)));
-                    }
-                }
-            }
-            */
         }
-
-        /*
-        private Brush GetLineBackgroundBrush(DiffPiece line)
-        {
-            var highlight = Brushes.Transparent;
-            switch (line.Type)
-            {
-                case ChangeType.Inserted:
-                    highlight = Brushes.Green;
-                    break;
-                case ChangeType.Deleted:
-                    highlight = Brushes.Red;
-                    break;
-                case ChangeType.Modified:
-                    highlight = Brushes.Yellow;
-                    break;
-            }
-
-            return highlight;
-        }
-        */
-
     }
 }
